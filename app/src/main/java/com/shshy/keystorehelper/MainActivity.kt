f910aa90ac72ec05363343aebd17ffb3eb36f2b2
ShiShY <616551624@qq.com>
2020-04-09 18:00:56 +0800

    添加分段加解密方法

--- app/src/main/java/com/shshy/keystorehelper/MainActivity.kt
+++ app/src/main/java/com/shshy/keystorehelper/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.shshy.keystorelib.KeyStoreRSA
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,19 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         KeyStoreRSA.getInstance().generateKey(this)
+        val strBuilder = StringBuilder()
+        for (i in 0 until 100)
+            strBuilder.append("shishaoyang我是啊发发的+-/}{")
         val encryptedData =
-            KeyStoreRSA.getInstance().encrypt("shishaoyang我是啊发发的+-/}{".toByteArray())
+            KeyStoreRSA.getInstance().encryptSpilt(strBuilder.toString().toByteArray())
         encryptedData?.let {
-            Log.e("aaa", String(it))
-            val decryptedData = KeyStoreRSA.getInstance().decrypt(it)
-            Log.e("aaa", String(decryptedData ?: ByteArray(0)))
+            val decryptedData = KeyStoreRSA.getInstance().decryptSpilt(it)
+            Log.e("aaa", "${strBuilder.toString() == String(decryptedData ?: ByteArray(0))}")
         }
 
-        val signData = KeyStoreRSA.getInstance().sign("shishaoyang我是啊发发的+-/}{".toByteArray())
+        val signData = KeyStoreRSA.getInstance().sign(strBuilder.toString().toByteArray())
         signData?.let {
             Log.e("aaa", String(it))
             val verify =
-                KeyStoreRSA.getInstance().verify("shishaoyang我是啊发发的+-/}{".toByteArray(), it)
+                KeyStoreRSA.getInstance().verify(strBuilder.toString().toByteArray(), it)
             Log.e("aaa", "$verify")
         }
     }
--- keystorelib/src/main/java/com/shshy/keystorelib/KeyStoreRSA.kt
+++ keystorelib/src/main/java/com/shshy/keystorelib/KeyStoreRSA.kt
@@ -39,6 +39,8 @@ class KeyStoreRSA private constructor() {
     private val CIPHER_TRANSFORMATION = "RSA/ECB/PKCS1Padding"
     private val defaultAlias = "shshydev"
     private val DEFAULT_KEY_SIZE = 2048
+    private val DEFAULT_BUFFERSIZE = (DEFAULT_KEY_SIZE / 8) - 11
+    private val DEFAULT_SPLIT = "#AAAAAAAA#".toByteArray()
 
     init {
         keyStore.load(null)
@@ -144,6 +146,129 @@ class KeyStoreRSA private constructor() {
         } catch (e: Exception) {
             e.printStackTrace()
             false
+        }
+    }
+
+    /**
+     * 用公钥对字符串进行分段加密
+     */
+    fun encryptSpilt(data: ByteArray, alias: String = defaultAlias): ByteArray? {
+        return try {
+            val dataLen = data.size
+            if (dataLen <= DEFAULT_BUFFERSIZE) {
+                return encrypt(data, alias)
+            }
+            val allBytes = ArrayList<Byte>(2048)
+            var bufIndex = 0
+            var subDataLoop = 0
+            var buf: ByteArray? = ByteArray(DEFAULT_BUFFERSIZE)
+            for (i in 0 until dataLen) {
+                buf?.let {
+                    it[bufIndex] = data[i]
+                    if (++bufIndex == DEFAULT_BUFFERSIZE || i == dataLen - 1) {
+                        subDataLoop++
+                        if (subDataLoop != 1) {
+                            for (b in DEFAULT_SPLIT) {
+                                allBytes.add(b)
+                            }
+                        }
+                        var encryptBytes = encrypt(it, alias)
+                        encryptBytes?.let {
+                            for (b in encryptBytes) {
+                                allBytes.add(b)
+                            }
+                        }
+                        bufIndex = 0
+                        buf = if (i == dataLen - 1) {
+                            null
+                        } else {
+                            ByteArray(DEFAULT_BUFFERSIZE.coerceAtMost(dataLen - i - 1))
+                        }
+                    }
+                }
+            }
+            val bytes = ByteArray(allBytes.size)
+            allBytes.forEachIndexed { index, byte ->
+                bytes[index] = byte
+            }
+            bytes
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    /**
+     * 使用私钥分段解密
+     */
+    fun decryptSpilt(encrypted: ByteArray, alias: String = defaultAlias): ByteArray? {
+        return try {
+            val splitLen = DEFAULT_SPLIT.size
+            if (splitLen <= 0) {
+                return decrypt(encrypted, alias)
+            }
+            val dataLen = encrypted.size
+            val allBytes = ArrayList<Byte>(1024)
+            var latestStartIndex = 0
+            var i = 0
+            while (i < dataLen) {
+                val bt = encrypted[i]
+                var isMatchSplit = false
+                if (i == dataLen - 1) {
+                    // 到data的最后了
+                    val part = ByteArray(dataLen - latestStartIndex)
+                    System.arraycopy(encrypted, latestStartIndex, part, 0, part.size)
+                    val decryptPart = decrypt(part, alias)
+                    decryptPart?.let {
+                        for (b in it) {
+                            allBytes.add(b)
+                        }
+                    }
+                    latestStartIndex = i + splitLen
+                    i = latestStartIndex - 1
+                } else if (bt == DEFAULT_SPLIT[0]) {
+                    // 这个是以split[0]开头
+                    if (splitLen > 1) {
+                        if (i + splitLen < dataLen) {
+                            // 没有超出data的范围
+                            for (j in 1 until splitLen) {
+                                if (DEFAULT_SPLIT[j] !== encrypted[i + j]) {
+                                    break
+                                }
+                                if (j == splitLen - 1) {
+                                    // 验证到split的最后一位，都没有break，则表明已经确认是split段
+                                    isMatchSplit = true
+                                }
+                            }
+                        }
+                    } else {
+                        // split只有一位，则已经匹配了
+                        isMatchSplit = true
+                    }
+                }
+                if (isMatchSplit) {
+                    val part = ByteArray(i - latestStartIndex)
+                    System.arraycopy(encrypted, latestStartIndex, part, 0, part.size)
+                    val decryptPart = decrypt(part, alias)
+                    decryptPart?.let {
+                        for (b in it) {
+                            allBytes.add(b)
+                        }
+                    }
+                    latestStartIndex = i + splitLen
+                    i = latestStartIndex - 1
+                }
+                i++
+            }
+
+            val bytes = ByteArray(allBytes.size)
+            allBytes.forEachIndexed { index, byte ->
+                bytes[index] = byte
+            }
+            bytes
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
         }
     }
 
